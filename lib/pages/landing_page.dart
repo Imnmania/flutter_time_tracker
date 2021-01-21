a4e4308890afd8594e25607445be432909eff1d1
Imnmania <niloyb@live.com>
2021-01-21 22:02:52 +0600

    home page added

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker/pages/home_page.dart';
 import 'package:time_tracker/pages/sign_in_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -11,7 +12,7 @@ class _LandingPageState extends State<LandingPage> {
   User _user;
 
   void _updateUser(User user) {
-    // print('User id: ${user.uid}');
+    print('User id: ${user.uid}');
     // print('Is anonymous: ${user.isAnonymous}');
     setState(() {
       _user = user;
@@ -26,6 +27,6 @@ class _LandingPageState extends State<LandingPage> {
         onSignIn: _updateUser,
       );
     }
-    return Container(); // temporary placeholder for homepage
+    return HomePage(); // temporary placeholder for homepage
   }
 }
