05fba5b1656756add945a2aea7add69731234eee
Imnmania <niloyb@live.com>
2021-01-22 01:28:06 +0600

    added a local user class for firebase user

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,16 +1,31 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class FBUser {
+  final String uid;
+
+  FBUser({@required this.uid});
+}
 
 class Auth {
   //
   final _firebaseAuth = FirebaseAuth.instance;
   //
-  Future<User> currentUser() async {
-    return _firebaseAuth.currentUser;
+  FBUser _userFromFirebase(User user) {
+    if (user == null) {
+      return null;
+    }
+    return FBUser(uid: user.uid);
   }
 
-  Future<User> signInAnonymously() async {
+  Future<FBUser> currentUser() async {
+    final user = _firebaseAuth.currentUser;
+    return _userFromFirebase(user);
+  }
+
+  Future<FBUser> signInAnonymously() async {
     final authResult = await _firebaseAuth.signInAnonymously();
-    return authResult.user;
+    return _userFromFirebase(authResult.user);
   }
 
   Future<void> signOut() async {
